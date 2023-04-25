e8c086d9c8893bfdc7cf3bce288022c569ec78e0
peter <2365333260@qq.com>
2023-04-25 18:05:50 +0800

    top alarm

--- alarm.py
+++ alarm.py
@@ -1,4 +1,5 @@
 import os
+import time
 import tkinter as tk
 from tkinter import messagebox
 
@@ -6,7 +7,10 @@ from tkinter import messagebox
 def alarm():
     root = tk.Tk()
     root.withdraw()
+    root.attributes("-topmost", True)
+
     messagebox.showinfo("Alarm", "坐正了")
+    #root.mainloop()
     print("Alarm!")
     #os.system("alarm.mp3")  # play alarm sound
     return 0
