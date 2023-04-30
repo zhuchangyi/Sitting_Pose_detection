a5b8e82916433d910172c25b7f606a2815112eee
peter <2365333260@qq.com>
2023-04-30 17:53:05 +0800

    save datasets to .csv

--- csv_convert.py
+++ csv_convert.py
@@ -15,8 +15,8 @@ def write_to_csv(data):
     else:
         print(f"Folder '{folder_name}' already exists.")
 
-    filename = input("请输入文件名：") + ".csv"
-    label = input("请输入标签：")
+    filename = input("Please enter the file name:") + ".csv"
+    label = input("Please enter the label:")
     with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['nose_x','nose_y','nose_score','left_eye_x','left_eye_y','left_eye_score','right_eye_x','right_eye_y','right_eye_score','left_ear_x','left_ear_y','left_ear_score','right_ear_x','right_ear_y','right_ear_score','left_shoulder_x','left_shoulder_y','left_shoulder_score','right_shoulder_x','right_shoulder_y','right_shoulder_score','left_elbow_x','left_elbow_y','left_elbow_score','right_elbow_x','right_elbow_y','right_elbow_score','left_wrist_x','left_wrist_y','left_wrist_score','right_wrist_x','right_wrist_y','right_wrist_score','left_hip_x','left_hip_y','left_hip_score','right_hip_x','right_hip_y','right_hip_score','left_knee_x','left_knee_y','left_knee_score','right_knee_x','right_knee_y','right_knee_score','left_ankle_x','left_ankle_y','left_ankle_score','right_ankle_x','right_ankle_y','right_ankle_score','class_no'
