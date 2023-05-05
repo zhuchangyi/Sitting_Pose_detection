05b4e198c0f8786cd226aefaf3ba186e4884fcd1
peter <2365333260@qq.com>
2023-05-05 17:43:10 +0800

    train your own model

--- training.py
+++ training.py
@@ -1,18 +1,10 @@
-import csv
-import cv2
 import itertools
 import numpy as np
 import pandas as pd
-import os
-import sys
-import tempfile
-import tqdm
 
 from matplotlib import pyplot as plt
-from matplotlib.collections import LineCollection
 
 import tensorflow as tf
-import tensorflow_hub as hub
 from tensorflow import keras
 
 from sklearn.model_selection import train_test_split
@@ -53,16 +45,9 @@ X_train, X_val, y_train, y_val = train_test_split(X, y,test_size=0.15)
 X_test, y_test= load_pose_landmarks('F:/MoveNet1/test_5_4.csv')
 
 def landmarks_to_embedding(landmarks_and_scores):
-  """Converts the input landmarks into a pose embedding."""
-  # Reshape the flat input into a matrix with shape=(17, 3)
   reshaped_inputs = keras.layers.Reshape((17, 3))(landmarks_and_scores)
-
-  # Normalize landmarks 2D
   landmarks = reshaped_inputs[:, :, :2]
-
-  # Flatten the normalized landmark coordinates into a vector
   embedding = keras.layers.Flatten()(landmarks)
-
   return embedding
 
 inputs = tf.keras.Input(shape=(51))
@@ -158,5 +143,49 @@ plot_confusion_matrix(cm,
                       title ='Confusion Matrix of Pose Classification Model')
 plt.show()
 # Print the classification report
-print('\nClassification Report:\n', classification_report(y_true_label,
-                                                          y_pred_label))
+print('\nClassification Report:\n', classification_report(y_true_label,y_pred_label))
+
+def model_convert():
+    model = tf.keras.models.load_model('F:\MoveNet1\weights.best.hdf5')
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    tflite_model = converter.convert()
+
+    print('Model size: %dKB' % (len(tflite_model) / 1024))
+
+    with open('pose_classifier.tflite', 'wb') as f:
+        f.write(tflite_model)
+
+
+def evaluate_model(interpreter, X, y_true):
+  """Evaluates the given TFLite model and return its accuracy."""
+  input_index = interpreter.get_input_details()[0]["index"]
+  output_index = interpreter.get_output_details()[0]["index"]
+
+  # Run predictions on all given poses.
+  y_pred = []
+  for i in range(len(y_true)):
+    # Pre-processing: add batch dimension and convert to float32 to match with
+    # the model's input data format.
+    test_image = X[i: i + 1].astype('float32')
+    interpreter.set_tensor(input_index, test_image)
+
+    # Run inference.
+    interpreter.invoke()
+
+    # Post-processing: remove batch dimension and find the class with highest
+    # probability.
+    output = interpreter.tensor(output_index)
+    predicted_label = np.argmax(output()[0])
+    y_pred.append(predicted_label)
+
+  # Compare prediction results with ground truth labels to calculate accuracy.
+  y_pred = keras.utils.to_categorical(y_pred)
+  return accuracy_score(y_true, y_pred)
+
+model_convert()
+tflite_model = open('pose_classifier.tflite', 'rb').read()
+classifier_interpreter = tf.lite.Interpreter(model_content=tflite_model)
+classifier_interpreter.allocate_tensors()
+print('Accuracy of TFLite model: %s' %
+      evaluate_model(classifier_interpreter, X_test, y_test))
