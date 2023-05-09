8dbcd7bb673fba82d7efd81ef77d093747f82903
zhuchangyi <51321351+zhuchangyi@users.noreply.github.com>
2023-05-09 15:44:21 +0800

    .csv save and merge

--- csv_convert.py
+++ csv_convert.py
@@ -1,8 +1,9 @@
 import csv
 import numpy as np
 import os
+import pandas as pd
 
-def write_to_csv(data):
+def folder_create():
     folder_name = "datas"
 
     if not os.path.isdir(folder_name):
@@ -15,7 +16,11 @@ def write_to_csv(data):
     else:
         print(f"Folder '{folder_name}' already exists.")
 
-    filename = input("Please enter the file name:") + ".csv"
+def write_to_csv(data):
+
+
+    folder_create()
+    filename = "datas/train/" + input("Please enter the file name:") + ".csv"
     label = input("Please enter the label:")
     with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
@@ -25,7 +30,23 @@ def write_to_csv(data):
             row = np.append(row, label)
             writer.writerow(row)
         f.close()
+    if input("Do you want to merge all datas?(y/n)") == 'y':
+        merge_csv()
 
-# data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]#for test
-# data = np.array(data)
+def merge_csv():
+    folder_path = "datas/train/"
+    csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+
+   
+    dfs = []
+    for file in csv_files:
+        file_path = os.path.join(folder_path, file)
+        df = pd.read_csv(file_path)
+        dfs.append(df)
+
+    # 将所有 DataFrame 合并为一个，并保存到 CSV 文件
+    merged_df = pd.concat(dfs, ignore_index=True)
+    merged_df.to_csv("datas/train/train.csv", index=False)
+# data =np.random.rand(1,51)
 # write_to_csv(data)
+#merge_csv()
